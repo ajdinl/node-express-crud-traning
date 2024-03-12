aa66fa18e136db7de275fcf677c99d452c29d4fc
ajdinl <ajdinl@live.com>
2024-03-12 15:02:24 +0100

    implement cors, route, and not found route

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express()
 const connectDB = require('./config/db')
 const port = process.env.PORT || 3000
+const cors = require('cors')
+const users = require('./routes/userRoutes')
 
 connectDB()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/api/users', users)
+
+app.use((req, res) => {
+  res.status(404).send('Not Found')
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
