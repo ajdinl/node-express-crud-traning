629cc83d4f2ab3323ccf36c994b7373888151399
ajdinl <ajdinl@live.com>
2024-03-12 14:42:51 +0100

    connect to db

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const connectDB = require('./config/db')
+const port = process.env.PORT || 3000
+
+connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
