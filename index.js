b2aadf2e9b71510ab0d7ba6d5c545254f8d44fcf
ajdinl <ajdinl@live.com>
2024-03-13 12:02:55 +0100

    create get and post route for postgres db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const connectDB = require('./config/db')
+const { pool, connectDB } = require('./config/db')
 const port = process.env.PORT || 3000
 const cors = require('cors')
 const users = require('./routes/userRoutes')
@@ -9,10 +9,40 @@ const { errorHandler } = require('./middleware/errorHandler')
 
 connectDB()
 
+pool.connect((err) => {
+  if (err) console.error('connection error', err.stack)
+  console.log('Postgres connected')
+})
+
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.get('/', async (req, res) => {
+  try {
+    const posts = await pool.query('SELECT * FROM posts')
+    res.json(posts.rows)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const { id, title, description } = req.body
+    const newPost = await pool.query(
+      'INSERT INTO posts (id, title, description) VALUES ($1, $2, $3) RETURNING *',
+      [id, title, description]
+    )
+
+    res.json(newPost.rows[0])
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
 
 app.use('/api/users', users)
 app.use('/api/posts', posts)
